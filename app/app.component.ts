a515c3cdaf5f9881e11200082c41f8d0577549b1
Longfld <Longfld@users.noreply.github.com>
2017-10-18 10:00:54 +1100

    Update app.component.ts

--- app/app.component.ts
+++ app/app.component.ts
@@ -10,8 +10,8 @@ declare var ethers: any;
 export class AppComponent {
 
   protected form: FormGroup;
-  private myaddress:string;
-  private wbalance:string;
+  private Address:string;
+  private Balance:string;
   private Wallet :any;
   private providers :any;
   private provider :any;
@@ -21,26 +21,20 @@ export class AppComponent {
     this.Wallet = ethers.Wallet;
     this.providers = ethers.providers
     this.provider = this.providers.getDefaultProvider();
-    this.wbalance = "";
+    this.Balance = "";
   }
 
   ngOnInit() {
-/*
-    let Wallet = ethers.Wallet;
-    let privateKey = "0x0123456789012345678901234567890123456789012345678901234567890123";
-    let mywallet = new Wallet(privateKey);
-    console.log("Address: " + mywallet.address);
-    console.log("privateKey: " + mywallet.privateKey);
-*/
     
   }
 
   private Createform() {
     this.form = this.fb.group({
       myaddress: new FormControl(""),
-      myphrase: new FormControl(""),
-      username: new FormControl(""),
-      pwd: new FormControl(""),
+      myphrase: new FormControl("radar blur cabbage chef fix engine embark joy scheme fiction master release"),
+      username: new FormControl("support@ethers.io"),
+      pwd: new FormControl("password123"),
+      address: new FormControl(""),
       balance: new FormControl(""),
     });
   }
@@ -49,36 +43,21 @@ export class AppComponent {
   
     let privateKey = "0x0123456789012345678901234567890123456789012345678901234567890123";
     let mywallet = new this.Wallet(privateKey);
-    console.log("Address: " + mywallet.address);
     console.log("privateKey: " + mywallet.privateKey);
     //this.GetBalance(mywallet.address);
-
-    this.provider.getBalance(mywallet.address).then(function(bal){
-      // balance is a BigNumber (in wei); format is as a sting (in ether)
-
-       //this.wbalance 
-       var etherString = ethers.utils.formatEther(bal);
-       console.log("Balance: " + etherString);
-    }(this));
-
+    this.GetBalance(mywallet.address);
+   
   }
 
   private getM() {
     //Mnemonic Phrase
     let mnemonic = "radar blur cabbage chef fix engine embark joy scheme fiction master release";
     let mwallet = this.Wallet.fromMnemonic(mnemonic);
-    console.log("Address(mnemonic): " + mwallet.address);
+  
     console.log("privateKey(mnemonic): " + mwallet.privateKey);
   
-    //this.GetBalance(mwallet.address);
-    let mproviders = ethers.providers
-    let mprovider = mproviders.getDefaultProvider();
-
-    mprovider.getBalance(mwallet.address).then( b => {
-      var etherString = ethers.utils.formatEther(b);
-      console.log("Balance: " + etherString);
-       this.wbalance = etherString;
-    })
+    this.GetBalance(mwallet.address);
+    
   }
 
    //Brain Wallet
@@ -87,20 +66,19 @@ export class AppComponent {
     let password = "password123";
     
     this.Wallet.fromBrainWallet(username, password).then(wallet => {
-      console.log("Address(Brain): " + wallet.address);
       console.log("Private Key(Brain): " + wallet.privateKey);
-      // "Address: 0x7Ee9AE2a2eAF3F0df8D323d555479be562ac4905"
       this.GetBalance(wallet.address);
     });
 
   }
 
   private GetBalance(address:any){
+    this.form.controls["address"].patchValue(address)
     this.provider.getBalance(address).then( bal =>{
       // balance is a BigNumber (in wei); format is as a sting (in ether)
-
-       this.wbalance = ethers.utils.formatEther(bal);
-       console.log("Balance: " + this.wbalance);
+       this.Balance = ethers.utils.formatEther(bal);
+       this.form.controls["balance"].patchValue(this.Balance)
+      
     });
   }
 }
