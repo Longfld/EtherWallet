014db520530efe1427a14bb783d86697c322f098
Longfld <Longfld@users.noreply.github.com>
2017-10-17 17:10:33 +1100

    Update app.module.ts

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,10 +1,10 @@
-import { NgModule }      from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-
-import { AppComponent }   from './app.component';
+import {NgModule}      from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {FormsModule, ReactiveFormsModule}   from '@angular/forms';
+import {AppComponent }   from './app.component';
 
 @NgModule({
-  imports:      [ BrowserModule ],
+  imports:      [ BrowserModule,ReactiveFormsModule ],
   declarations: [ AppComponent ],
   bootstrap:    [ AppComponent ]
 })
