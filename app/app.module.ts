61badcd04bf9d0d84d1271912d0c31980f80f27e
Longfld <Longfld@users.noreply.github.com>
2017-10-19 10:34:29 +1100

    Update app.module.ts

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,11 +1,17 @@
 import {NgModule}      from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 import {FormsModule, ReactiveFormsModule}   from '@angular/forms';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {AppComponent }   from './app.component';
+import {EwalletMaterialModule}  from './ewallet.material.module';
+
+import 'hammerjs/hammer';
 
 @NgModule({
-  imports:      [ BrowserModule,ReactiveFormsModule ],
+  imports:      [ BrowserModule,BrowserAnimationsModule,ReactiveFormsModule,
+                  EwalletMaterialModule ],
   declarations: [ AppComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
+
