af31d53e5d1e41a42ca639b4871c4dd498ab58ba
Longfld <Longfld@users.noreply.github.com>
2017-10-19 10:27:15 +1100

    Update systemjs.config.js

--- systemjs.config.js
+++ systemjs.config.js
@@ -1,7 +1,3 @@
-/**
- * System configuration for Angular samples
- * Adjust as necessary for your application needs.
- */
 (function (global) {
   System.config({
     paths: {
@@ -20,13 +16,30 @@
       '@angular/platform-browser-dynamic': 'npm:@angular/platform-browser-dynamic/bundles/platform-browser-dynamic.umd.js',
       '@angular/http': 'npm:@angular/http/bundles/http.umd.js',
       '@angular/router': 'npm:@angular/router/bundles/router.umd.js',
-      '@angular/router/upgrade': 'npm:@angular/router/bundles/router-upgrade.umd.js',
+      '@angular/animations/browser': 'npm:@angular/animations/bundles/animations-browser.umd.js',
+      '@angular/platform-browser/animations': 'npm:@angular/platform-browser/bundles/platform-browser-animations.umd.js',
       '@angular/forms': 'npm:@angular/forms/bundles/forms.umd.js',
-      '@angular/upgrade': 'npm:@angular/upgrade/bundles/upgrade.umd.js',
-      '@angular/upgrade/static': 'npm:@angular/upgrade/bundles/upgrade-static.umd.js',
+      '@angular/animations': 'npm:@angular/animations/bundles/animations.umd.js',
+      //material
+      '@angular/material': 'npm:@angular/material/bundles/material.umd.js',
+      '@angular/cdk': 'npm:@angular/cdk/bundles/cdk.umd.js',
+      '@angular/cdk/a11y': 'npm:@angular/cdk/bundles/cdk-a11y.umd.js',
+      '@angular/cdk/bidi': 'npm:@angular/cdk/bundles/cdk-bidi.umd.js',
+      '@angular/cdk/coercion': 'npm:@angular/cdk/bundles/cdk-coercion.umd.js',
+      '@angular/cdk/collections': 'npm:@angular/cdk/bundles/cdk-collections.umd.js',
+      '@angular/cdk/keycodes': 'npm:@angular/cdk/bundles/cdk-keycodes.umd.js',
+      '@angular/cdk/observers': 'npm:@angular/cdk/bundles/cdk-observers.umd.js',
+      '@angular/cdk/overlay': 'npm:@angular/cdk/bundles/cdk-overlay.umd.js',
+      '@angular/cdk/platform': 'npm:@angular/cdk/bundles/cdk-platform.umd.js',
+      '@angular/cdk/portal': 'npm:@angular/cdk/bundles/cdk-portal.umd.js',
+      '@angular/cdk/rxjs': 'npm:@angular/cdk/bundles/cdk-rxjs.umd.js',
+      '@angular/cdk/scrolling': 'npm:@angular/cdk/bundles/cdk-scrolling.umd.js',
+      '@angular/cdk/table': 'npm:@angular/cdk/bundles/cdk-table.umd.js',
+      '@angular/cdk/stepper': 'npm:@angular/cdk/bundles/cdk-stepper.umd.js',
+  
       // other libraries
       'rxjs':                      'npm:rxjs',
-      'angular-in-memory-web-api': 'npm:angular-in-memory-web-api/bundles/in-memory-web-api.umd.js'
+      'hammerjs': 'npm:hammerjs'
     },
     // packages tells the System loader how to load when no filename and/or no extension
     packages: {
@@ -35,6 +48,10 @@
         defaultExtension: 'js'
       },
       rxjs: {
+        defaultExtension: 'js'
+      },
+      hammerjs:{
+        main: './hammer.js', 
         defaultExtension: 'js'
       }
     }
